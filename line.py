c0f882202c78d1373c1723272a8f7007b4cc0757
Gael2323 <lgherrera@mail.austral.edu.ar>
2025-04-18 16:41:45 -0300

    Update line.py

--- line.py
+++ line.py
@@ -18,4 +18,4 @@ def line():
     print(f"\tP2 ({X2}, {A * X2 +B})")
     
     dist = ((X2 - X1)**2 + (Y2 - Y1)**2)**0.5
-    print(f"\n La distancia entre ellos es: {dist}")
+    print(f"\nLa distancia entre ellos es: {dist}")
